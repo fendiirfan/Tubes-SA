580ff04f80507506783b899cababe7de90287ccf
Kaenova Mahendra Auditama <kaenova@gmail.com>
2021-06-04 20:23:14 +0700

    Editting on main.py :: changing getMatrixLink to foot-hiking :: adding more comments in the algorithm :: printing route inline

--- main.py
+++ main.py
@@ -15,15 +15,15 @@ load_dotenv(dotenv_path)
 
 API_KEY = os.environ.get("API")
 getCoordinateLink = (
-    "https://api.openrouteservice.org/geocode/search?api_key={}&text=".format(API_KEY)
-)
-getMatrixLink = ""
+    "https://api.openrouteservice.org/geocode/search?api_key={}&text=".format(
+        API_KEY))
+getMatrixLink = "https://api.openrouteservice.org/v2/matrix/foot-hiking"
 
 
-# =========================================== API THINGY ==============================================
-# printing on json format
+# =========================================== API ==============================================
 def jprint(obj):
-    # create a formatted string of the Python JSON object
+    # IS: Terdefinisi sebuah objek
+    # FS: Mengoutputkan isi dari objek-objek tersebut dengan bentuk json
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
@@ -60,13 +60,14 @@ def getMatrixDistance(cities_coordinate_array):
     }
 
     headers = {
-        "Accept": "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8",
+        "Accept":
+        "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8",
         "Authorization": API_KEY,
         "Content-Type": "application/json; charset=utf-8",
     }
 
     response = requests.post(
-        "https://api.openrouteservice.org/v2/matrix/foot-walking",
+        getMatrixLink,
         json=body,
         headers=headers,
     )
@@ -83,7 +84,9 @@ def getMatrixDistance(cities_coordinate_array):
     return res_json["distances"]
 
 
-def printAllInpuutedCities(list_city):
+def printAllInputedCities(list_city):
+    # IS: Terdefinisi sebuah list yang berisi objek tempat
+    # FS: Mengoutpukan data yang disimpan pada objek tempat
     for i in range(len(list_city)):
         print("")
         print("City Number {}".format(i + 1))
@@ -92,30 +95,36 @@ def printAllInpuutedCities(list_city):
         print("Cities Name: {}".format(list_city[i][0]))
         print("Coordinate: {}".format(list_city[i][1]))
         if len(list_city[i]) == 3:
-            print(
-                "Jarak ke destinasi lain berdasarkan index: {}".format(list_city[i][2])
-            )
+            print("Jarak ke destinasi lain berdasarkan index: {}".format(
+                list_city[i][2]))
 
 
 # =========================================== GREEDY ALGORITHM ==============================================
 class tempat:
-    # Suatu tempat terdiri dari id, nama, dan jarak destinasi dari tempat tersebut ke tempat lainnya
-    def __init__(self, id: int, nama="null", koordinat_kota=[], jarak_destinasi=[]):
+    # Objek "Tempat"
+    # Objek ini memiliki properti id, nama kota, jarak dari kota tersebut ke kota lainnya dalam bentuk list yang ditentukan dengan index relatif ke variable city_list_for_greedy
+    def __init__(self,
+                 id: int,
+                 nama="null",
+                 koordinat_kota=[],
+                 jarak_destinasi=[]):
+        # Inisialisasi objek, dengan properti id, nama, list jarak dari kota yang terinisialisasi dengan kota-kota lainnya, dan koordinat
         self.id = id
         self.nama = nama
         self.jarak = jarak_destinasi
         self.koordinat = koordinat_kota
 
     def jarakMinimumSelainList(self, list_rute):
-        # IS: list rute terdefinisi dari rute yang sudah dibuat
-        # FS: mengembalikan index, dan nilai terkecil dari jarak selain index yang sudah di definisikan dari list_rute
-        minimum = -1  # index
-        # list rute = [0, 1, 2, 3]
+        # IS: list rute terdefinisi dari rute-rute yang sudah dibuat
+        # FS: mengembalikan index suatu kota yang tidak terdefinisi pada list_rute dengan nilai jarak terkecil dari properti jarak yang dimiliki objek ini.
+
+        minimum = -1
         for i in range(len(self.jarak)):
             if self.jarak[i] != 0:
                 if minimum == -1 and (i not in list_rute):
                     minimum = i
-                if (self.jarak[i] <= self.jarak[minimum]) and (i not in list_rute):
+                if (self.jarak[i] <=
+                        self.jarak[minimum]) and (i not in list_rute):
                     minimum = i
 
         print("Jarak Minimum dari Kota {}".format(self.nama))
@@ -129,60 +138,67 @@ class tempat:
         return [minimum, self.jarak[minimum]]  # (index, jarak_aktual)
 
     def printTempat(self):
-        print("id: {}\nNama: {}\n    Jarak: {}".format(self.id, self.nama, self.jarak))
+        print("id: {}\nNama: {}\n    Jarak: {}".format(self.id, self.nama,
+                                                       self.jarak))
 
 
 def greedy(list_destinasi):
     # IS: Terdefinisi list_destiniasi yang merupakan list dari tempat-tempat yang sudah didefinisikan
-    # FS: Mengembalikan list yang berisi pola sirkuit terpendek untuk menuju suatu tempat2.
+    # FS: Mengembalikan list yang berisi nomor index pola sirkuit terpendek untuk membuat sirkui tempat-tempat yang sudah terdefinisi.
 
-    # list_destinasi merupakan list yang berisi objek destinasi original
-    # rute_akhir merupakan list yang berisi pola hasil greedy yang berbentuk index dari list_destinasi
+    # list_destinasi merupakan list variabel yang berisi objek tempat
+    # [objekTempat1, objekTempat2, objekTempat3]
+    
+    # rute_akhir merupakan list yang berisi pola hasil greedy, berisi dari nomor index kota relatif ke list list_destinasi
+    # [0, 2 ,1] <- nomor index relatif kepada list_destinasi
+    # artinya, kalau misalkan digambarkan nomor-nomor index tersebut digambarkan menjadi objek aslinya, sehingga
+    # ["objekTempat1", "objekTempat3", "objekTempat2"]
+
     print("\n\n")
     print(Fore.RED, "GREEDY BY DISTANCE", Fore.WHITE)
-    rute_akhir = [0]  # [1, 0, 2, 3, 4, 5]
-    for i in range(len(list_destinasi) - 1):
+    rute_akhir = [
+        0
+    ]  # diawali dengan kota index ke 0 relatif dari list_destinasi.
+
+    for i in range(len(list_destinasi) - 1):  # perulangan akan dilakukan sebanyak jumlah kota yang terdefinisi - 1
         print(Fore.RED, "=======================", Fore.WHITE)
         print("ITERASI {}".format(i + 1))
-        if len(rute_akhir) == 1:
-            next_destinasi_kanan = list_destinasi[rute_akhir[0]].jarakMinimumSelainList(
-                rute_akhir
-            )
-            print(
-                "Terpilih {} dari {} dengan jarak {}".format(
+
+        if len(rute_akhir) == 1:                                            # Ketika hanya 1 kota yang ada di rute_akhir
+            next_destinasi_kanan = list_destinasi[rute_akhir[0]].jarakMinimumSelainList(rute_akhir)
+            print("Terpilih {} dari {} dengan jarak {}".format(
+                list_destinasi[next_destinasi_kanan[0]].nama,
+                list_destinasi[rute_akhir[len(rute_akhir) - 1]].nama,
+                next_destinasi_kanan[1],
+            ))
+            rute_akhir.append(next_destinasi_kanan[0])
+        else:                                                               # Ketika sudah lebih dari 1 kota yang terdefinisi di rute_akhir
+            idx_kota_node_kanan = rute_akhir[len(rute_akhir) - 1]
+            idx_kota_node_kiri = rute_akhir[0]
+            
+            print("Node Kanan")
+            next_destinasi_kanan = list_destinasi[idx_kota_node_kanan].jarakMinimumSelainList(rute_akhir)
+            # next_destinasi_kanan akan mendapatkan index kota yang terpendek relatif dari kota yang berada pada list rute_akhir di paling akhir dan jarak dari kota tersebut ke kota selanjutnya
+            print("Node Kiri")
+            next_destinasi_kiri = list_destinasi[idx_kota_node_kiri].jarakMinimumSelainList(rute_akhir)
+            # next_destinasi_kiri akan mendapatkan index kota yang terpendek relatif dari kota yang berada pada list rute_akhir di paling awal dan jarak dari kota tersebut ke kota selanjutnya
+            
+            # Pemilihan dari node kanan dan node kiri, mana yang memiliki jarak untuk destinasi selanjutnya yang paling kecil
+            if next_destinasi_kanan[1] >= next_destinasi_kiri[1]:
+                print("Terpilih {} dari {} dengan jarak {}".format(
+                    list_destinasi[next_destinasi_kiri[0]].nama,
+                    list_destinasi[rute_akhir[0]].nama,
+                    next_destinasi_kiri[1],
+                ))
+                rute_akhir.insert(0, next_destinasi_kiri[0])
+            else:
+                print("Terpilih {} dari {} dengan jarak {}".format(
                     list_destinasi[next_destinasi_kanan[0]].nama,
                     list_destinasi[rute_akhir[len(rute_akhir) - 1]].nama,
                     next_destinasi_kanan[1],
-                )
-            )
-            rute_akhir.append(next_destinasi_kanan[0])
-        else:
-            print("Node Kanan")
-            next_destinasi_kanan = list_destinasi[
-                rute_akhir[len(rute_akhir) - 1]
-            ].jarakMinimumSelainList(rute_akhir)
-            print("Node Kiri")
-            next_destinasi_kiri = list_destinasi[rute_akhir[0]].jarakMinimumSelainList(
-                rute_akhir
-            )
-            if next_destinasi_kanan[1] >= next_destinasi_kiri[1]:
-                print(
-                    "Terpilih {} dari {} dengan jarak {}".format(
-                        list_destinasi[next_destinasi_kiri[0]].nama,
-                        list_destinasi[rute_akhir[0]].nama,
-                        next_destinasi_kiri[1],
-                    )
-                )
-                rute_akhir.insert(0, next_destinasi_kiri[0])
-            else:
-                print(
-                    "Terpilih {} dari {} dengan jarak {}".format(
-                        list_destinasi[next_destinasi_kanan[0]].nama,
-                        list_destinasi[rute_akhir[len(rute_akhir) - 1]].nama,
-                        next_destinasi_kanan[1],
-                    )
-                )
+                ))
                 rute_akhir.append(next_destinasi_kanan[0])
+                
     print(Fore.BLUE, "============== GREEDY FINISH ==============", Fore.WHITE)
     return rute_akhir
 
@@ -216,6 +232,7 @@ if __name__ == "__main__":
                     input_counter += 1
                 elif input_city == "del":
                     city_list.pop(len(city_list) - 1)
+                    input_counter -= 1
                 else:
                     bool_input_ulang = False
             except:
@@ -235,7 +252,7 @@ if __name__ == "__main__":
             input_cities_state = False
             for i in range(len(city_list)):
                 city_list[i].append(matrix_jarak[i])
-            printAllInpuutedCities(city_list)
+            printAllInputedCities(city_list)
         else:
             print("==================================================")
             print("Input Kota Tidak Valid, Coba untuk lebih spesifik")
@@ -245,7 +262,8 @@ if __name__ == "__main__":
 
     print("List Kota")
     for i in range(len(city_list)):
-        destinasi_temp = tempat(i, city_list[i][0], city_list[i][1], city_list[i][2])
+        destinasi_temp = tempat(i, city_list[i][0], city_list[i][1],
+                                city_list[i][2])
         city_list_for_greedy.append(destinasi_temp)
 
     print("=======================================")
@@ -256,5 +274,13 @@ if __name__ == "__main__":
 
     print("\n\n Pola Kota yang Didapatkan")
     print("=======================================")
+    print("Rute by ID:", end=" ")
+    for i in range(len(list_pola_destinasi_greedy)):
+        if i != (len(list_pola_destinasi_greedy) - 1):
+            print(list_pola_destinasi_greedy[i],end="-")
+        else:
+            print(str(list_pola_destinasi_greedy[i]) + "-" + str(list_pola_destinasi_greedy[0]))
     for i in range(len(list_pola_destinasi_greedy)):
         city_list_for_greedy[list_pola_destinasi_greedy[i]].printTempat()
+        
+    
